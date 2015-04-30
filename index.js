ee1dc97718a5d5cb759d8c1705ebcee01193e1f0
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-04-30 12:11:59 +0300

    acorn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var parse = require('acorn').parse;
+
 module.exports = function halstead(source) {
+	var tree = parse(source, { ecmaVersion: 6 });
+
+	// TODO: implement me
+
 	return { difficulty: 0, volume: 0, effort: 0 };
 };
